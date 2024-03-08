7e87040c5db0277a5c762c0987317597761bb980
guilhermeoli19 <162708565+guilhermeoli19@users.noreply.github.com>
2024-03-08 14:43:00 +0000

    Adicionar Exercícios Semana 1

---
+++ Semana1/exercicio1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e ");
+  printf("Prog. II\n");
+  printf("Estudaremos a linguagem C\n");
+  return 0;
+}
---
+++ Semana1/exercicio2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("Mackenzie - FCI\n");
+  printf("Algoritmos e ");
+  printf("Prog. II\n");
+  printf("Estudaremos a linguagem C\n");
+  printf("A FCI foi fundada no ano de 1970\n");
+  return 0;
+}
